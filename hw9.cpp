43414a19fbc16b9b5db3b313ee4b074e92764c9e
3A713212 <48340382+3A713212@users.noreply.github.com>
2019-04-12 08:42:40 +0800

    Add files via upload

---
+++ hw9.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h> 
+#include<stdlib.h>
+int main()
+{
+	int j,i; 
+	for(i=1;i<=9;i++)
+	{
+	
+	for(j=1;j<=9;j++){
+		printf("%d*%d=%d\t",i,j,2*j);
+		
+	}
+	printf("\n");
+}
+	
+
+return 0;
+
+
+	
+}
